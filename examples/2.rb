757f5a1340f90418ecc491d29a8f3085ffd462bf
manyrus <manyrus@gmail.com>
2014-06-07 01:03:31 +0400

    Some few fixes, example updated

--- examples/2.rb
+++ examples/2.rb
@@ -9,4 +9,8 @@ pupil.passport = '453312'
 pupil.region = '45'
 
 ege_parser = EgeParser::Parser.new
-p ege_parser.get_subjects pupil
+begin
+	p ege_parser.get_subjects pupil #output as {'subject'=>'status', 'subject#2'=>'status2'}
+rescue EgeParser::AuthError #see all errors in error.rb
+	p 'Auth error('
+end
